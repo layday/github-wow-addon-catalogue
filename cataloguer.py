a5952db7e6d7895d7032e0f3bbb5716a4a9754bb
layday <layday@protonmail.com>
2023-02-24 11:06:54 +0200

    Replace async with sync db
    Deadlocks be gone.

--- cataloguer.py
+++ cataloguer.py
@@ -8,11 +8,12 @@ import json
 import logging
 import os
 import re
+import sqlite3
 from collections import defaultdict
 from collections.abc import Iterable, Mapping, Sequence
-from contextlib import AbstractAsyncContextManager, asynccontextmanager
+from contextlib import AbstractAsyncContextManager, asynccontextmanager, contextmanager
 from dataclasses import dataclass, fields
-from datetime import datetime, timedelta, timezone
+from datetime import UTC, datetime, timedelta
 from itertools import chain, dropwhile, pairwise
 from typing import Any, Literal, NewType, Protocol
 from zipfile import ZipFile
@@ -20,13 +21,105 @@ from zipfile import ZipFile
 import aiohttp
 import click
 import structlog
-from aiohttp_client_cache.backends.sqlite import SQLiteBackend
+from aiohttp_client_cache import BaseCache, CacheBackend
+from aiohttp_client_cache.cache_control import ExpirationPatterns
 from aiohttp_client_cache.session import CachedSession
 from cattrs import BaseValidationError, Converter
 from cattrs.preconf.json import configure_converter as configure_json_converter
 from yarl import URL
 
 logger = structlog.get_logger()
+
+
+@contextmanager
+def get_sqlite_cache(db_path: str, cache: CacheBackend):
+    with sqlite3.connect(db_path) as db_connection:
+        db_connection.execute("PRAGMA journal_mode = wal")
+        db_connection.execute("PRAGMA synchronous = normal")
+        cache.responses = _SQLiteSimpleCache().prepare("responses", db_connection)
+        cache.redirects = _SQLitePickleCache().prepare("redirects", db_connection)
+        yield cache
+
+
+class _SQLiteSimpleCache(BaseCache):
+    @contextmanager
+    def _transact(self):
+        try:
+            yield
+        except BaseException:
+            self._db_connection.rollback()
+        else:
+            self._db_connection.commit()
+
+    def prepare(self, table_name: str, db_connection: sqlite3.Connection):
+        self.table_name = table_name
+        self._db_connection = db_connection
+        self._db_connection.execute(
+            f'CREATE TABLE IF NOT EXISTS "{self.table_name}" (key PRIMARY KEY, value)'
+        )
+        return self
+
+    async def clear(self) -> None:
+        raise NotImplementedError
+
+    async def contains(self, key: str):
+        cursor = self._db_connection.execute(
+            f'SELECT 1 FROM "{self.table_name}" WHERE key = ?',
+            (key,),
+        )
+        (value,) = cursor.fetchone() or (0,)
+        return value
+
+    async def delete(self, key: str) -> None:
+        raise NotImplementedError
+
+    async def bulk_delete(self, keys: object) -> None:
+        raise NotImplementedError
+
+    async def keys(self):
+        cursor = self._db_connection.execute(f'SELECT key FROM "{self.table_name}"')
+        for (value,) in cursor:
+            yield value
+
+    async def read(self, key: str):
+        cursor = self._db_connection.execute(
+            f'SELECT value FROM "{self.table_name}" WHERE key = ?',
+            (key,),
+        )
+        (value,) = cursor.fetchone() or (None,)
+        return value
+
+    async def size(self):
+        cursor = self._db_connection.execute(f'SELECT COUNT(key) FROM "{self.table_name}"')
+        (value,) = cursor.fetchone()
+        return value
+
+    async def values(self):
+        cursor = self._db_connection.execute(f'SELECT value FROM "{self.table_name}"')
+        for (value,) in cursor:
+            yield value
+
+    async def write(self, key: str, item: Any):
+        with self._transact():
+            self._db_connection.execute(
+                f'INSERT OR REPLACE INTO "{self.table_name}" (key, value) VALUES (?, ?)',
+                (key, item),
+            )
+
+
+class _SQLitePickleCache(_SQLiteSimpleCache):
+    async def read(self, key: str):
+        return self.deserialize(await super().read(key))
+
+    async def values(self):
+        cursor = self._db_connection.execute(f'SELECT value FROM "{self.table_name}"')
+        for (value,) in cursor:
+            yield self.deserialize(value)
+
+    async def write(self, key: str, item: Any):
+        encoded_item = self.serialize(item)
+        if encoded_item:
+            await super().write(key, memoryview(encoded_item))
 
 
 USER_AGENT = "github-wow-addon-catalogue (+https://github.com/layday/github-wow-addon-catalogue)"
@@ -36,7 +129,7 @@ API_URL = URL("https://api.github.com/")
 _SEARCH_INTERVAL_HOURS = 2
 
 CACHE_INDEFINITELY = -1
-EXPIRE_URLS = {
+EXPIRE_URLS: ExpirationPatterns = {
     f"{API_URL.host}/search": timedelta(hours=_SEARCH_INTERVAL_HOURS),
     f"{API_URL.host}/repos/*/releases": timedelta(days=1),
 }
@@ -118,10 +211,16 @@ TOC_ALIASES = {
 }
 
 TOP_LEVEL_TOC_NAME_PATTERN = re.compile(
-    (
-        rf"^(?P<name>[^/]+)[/](?P=name)(?:[-_](?P<flavor>{'|'.join(map(re.escape, TOC_ALIASES))}))?\.toc$"
-    ),
-    flags=re.I,
+    rf"""
+        ^
+        (?P<name>[^/]+)
+        [/]
+        (?P=name)
+        (?:[-_](?P<flavor>{'|'.join(map(re.escape, TOC_ALIASES))}))?
+        \.toc
+        $
+    """,
+    flags=re.I | re.X,
 )
 
 INTERFACE_RANGES_TO_FLAVORS = {
@@ -355,90 +454,97 @@ async def parse_repo(get: Get, repo: Mapping[str, Any]):
             last_updated=datetime.fromisoformat(f"{release['published_at'].rstrip('Z')}+00:00"),
             flavors=ProjectFlavors(flavors),
             has_release_json=maybe_release_json_asset is not None,
-            last_seen=datetime.now(timezone.utc),
+            last_seen=datetime.now(UTC),
             **project_ids,
         )
 
 
 async def get_projects(token: str):
-    async with CachedSession(
-        cache=SQLiteBackend(
-            "http-cache.db",
+    with get_sqlite_cache(
+        "http-cache.db",
+        CacheBackend(
             expire_after=CACHE_INDEFINITELY,
             urls_expire_after=EXPIRE_URLS,
         ),
-        connector=aiohttp.TCPConnector(limit_per_host=8),
-        headers={
-            "Accept": "application/vnd.github.v3+json",
-            "Authorization": f"token {token}",
-            "User-Agent": USER_AGENT,
-        },
-        timeout=aiohttp.ClientTimeout(sock_connect=10, sock_read=10),
-    ) as client:
+    ) as cache:
+        async with CachedSession(
+            cache=cache,
+            connector=aiohttp.TCPConnector(limit_per_host=8),
+            headers={
+                "Accept": "application/vnd.github.v3+json",
+                "Authorization": f"token {token}",
+                "User-Agent": USER_AGENT,
+            },
+            timeout=aiohttp.ClientTimeout(sock_connect=10, sock_read=10),
+        ) as client:
 
-        @asynccontextmanager
-        async def get(url: str | URL):
-            for _ in range(5):
-                async with client.get(url) as response:
-                    logger.debug(
-                        f"fetching {response.url}"
-                        + (
-                            f"\n\t{response.headers['X-RateLimit-Remaining'] or '?'} requests"
-                            " remaining"
-                            if "X-RateLimit-Remaining" in response.headers
-                            else ""
-                        )
-                    )
-
-                    if response.status == 403:
-                        logger.error(await response.text())
-
-                        sleep_until_header = response.headers.get("X-RateLimit-Reset")
-                        if sleep_until_header:
-                            sleep_until = datetime.fromtimestamp(
-                                int(response.headers["X-RateLimit-Reset"])
-                            )
-                            sleep_for = max(0, (sleep_until - datetime.now()).total_seconds())
-                            logger.info(
-                                "rate limited after"
-                                f" {response.headers['X-RateLimit-Used']} requests, sleeping until"
-                                f" {sleep_until.time().isoformat()} for ({sleep_for} + 1)s"
-                            )
-                            await asyncio.sleep(sleep_for + 1)
-
-                    elif not response.ok:
+            @asynccontextmanager
+            async def get(url: str | URL):
+                for _ in range(5):
+                    async with client.get(url) as response:
                         logger.debug(
-                            f"request errored with status {response.status}; sleeping for 3s"
+                            f"fetching {response.url}"
+                            + (
+                                f"\n\t{response.headers['X-RateLimit-Remaining'] or '?'} requests"
+                                " remaining"
+                                if "X-RateLimit-Remaining" in response.headers
+                                else ""
+                            )
                         )
-                        await asyncio.sleep(3)
-                        continue
 
-                    else:
-                        yield response
-                        break
-            else:
-                response.raise_for_status()  # pyright: ignore[reportUnboundVariable]
+                        if response.status == 403:
+                            logger.error(await response.text())
 
-        deduped_repos = {
-            r["full_name"]: r
-            async for r in find_addon_repos(
-                get,
-                [
-                    ("code", "path:.github/workflows bigwigsmods packager"),
-                    ("code", "path:.github/workflows CF_API_KEY"),
-                    ("repositories", "topic:wow-addon"),
-                    ("repositories", "topics:>2 topic:world-of-warcraft topic:addon"),
-                ],
-            )
-            if not r["full_name"].startswith(REPO_EXCLUDES)
-        }
-        projects = {
-            r
-            for c in asyncio.as_completed([parse_repo(get, r) for r in deduped_repos.values()])
-            for r in (await c,)
-            if r
-        }
-        return projects
+                            sleep_until_header = response.headers.get("X-RateLimit-Reset")
+                            if sleep_until_header:
+                                sleep_until = datetime.fromtimestamp(  # noqa: DTZ006
+                                    int(response.headers["X-RateLimit-Reset"])
+                                )
+                                sleep_for = max(
+                                    0,
+                                    (sleep_until - datetime.now()).total_seconds(),  # noqa: DTZ005
+                                )
+                                logger.info(
+                                    "rate limited after"
+                                    f" {response.headers['X-RateLimit-Used']} requests, sleeping"
+                                    f" until {sleep_until.time().isoformat()} for"
+                                    f" ({sleep_for} + 1)s"
+                                )
+                                await asyncio.sleep(sleep_for + 1)
+
+                        elif not response.ok:
+                            logger.debug(
+                                f"request errored with status {response.status}; sleeping for 3s"
+                            )
+                            await asyncio.sleep(3)
+                            continue
+
+                        else:
+                            yield response
+                            break
+                else:
+                    response.raise_for_status()  # pyright: ignore[reportUnboundVariable]
+
+            deduped_repos = {
+                r["full_name"]: r
+                async for r in find_addon_repos(
+                    get,
+                    [
+                        ("code", "path:.github/workflows bigwigsmods packager"),
+                        ("code", "path:.github/workflows CF_API_KEY"),
+                        ("repositories", "topic:wow-addon"),
+                        ("repositories", "topics:>2 topic:world-of-warcraft topic:addon"),
+                    ],
+                )
+                if not r["full_name"].startswith(REPO_EXCLUDES)
+            }
+            projects = {
+                r
+                for c in asyncio.as_completed([parse_repo(get, r) for r in deduped_repos.values()])
+                for r in (await c,)
+                if r
+            }
+            return projects
 
 
 def log_run():
@@ -450,7 +556,7 @@ def log_run():
         except json.JSONDecodeError:
             orig_runs = []
 
-        now = datetime.now(timezone.utc)
+        now = datetime.now(UTC)
         combined_runs = set(
             chain(
                 orig_runs[-MIN_PRUNE_RUNS:],
